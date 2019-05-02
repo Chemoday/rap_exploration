6354c3f8bf3f84951e8926b5cff9dceea4354200
Chemoday <aleksandr.kuhhar@gmail.com>
2019-05-02 23:14:05 +0300

    random square assesment corrected

--- app/blueprints/kifa/forms.py
+++ app/blueprints/kifa/forms.py
@@ -14,10 +14,10 @@ class AssesmentForm(FlaskForm):
         ('5', '5'),
     ]
 
-    label_0 = RadioField('label_0', choices=choices, default='0', validators=[validators.DataRequired()]) #add a validator
-    label_1 = RadioField('label_1', choices=choices, default='0', validators=[validators.DataRequired()]) #add a validator
-    label_2 = RadioField('label_2', choices=choices, default='0', validators=[validators.DataRequired()]) #add a validator
-    label_3 = RadioField('label_3', choices=choices, default='0', validators=[validators.DataRequired()]) #add a validator
-    label_4 = RadioField('label_4', choices=choices, default='0', validators=[validators.DataRequired()]) #add a validator
-    label_5 = RadioField('label_5', choices=choices, default='0', validators=[validators.DataRequired()]) #add a validator
+    label_0 = RadioField('label_0', choices=choices, validators=[validators.DataRequired()]) #add a validator
+    label_1 = RadioField('label_1', choices=choices, validators=[validators.DataRequired()]) #add a validator
+    label_2 = RadioField('label_2', choices=choices, validators=[validators.DataRequired()]) #add a validator
+    label_3 = RadioField('label_3', choices=choices, validators=[validators.DataRequired()]) #add a validator
+    label_4 = RadioField('label_4', choices=choices, validators=[validators.DataRequired()]) #add a validator
+    label_5 = RadioField('label_5', choices=choices, validators=[validators.DataRequired()]) #add a validator
     submit = SubmitField('Analyze')
--- app/blueprints/kifa/views.py
+++ app/blueprints/kifa/views.py
@@ -1,4 +1,5 @@
-from flask import render_template, session, redirect, url_for, current_app, request
+from flask import render_template, session, redirect,\
+    url_for, current_app, request, flash
 
 from . import kifa_bp
 from .forms import AssesmentForm
@@ -6,8 +7,15 @@ from .forms import AssesmentForm
 from app.database.models import *
 
 @kifa_bp.route('/square_assessment', methods=['GET', 'POST'])
-def square_assessment():
+def assessment_random_square():
     form = AssesmentForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            rate_square(assessment_id=request.args.get('assessment_id'))
+        else:
+            flash('mark the fields')
+            return redirect(url_for('.assessment_random_square'))
+
     query = Assessment.select().where(Assessment.rated == False).order_by(fn.Random())
     random_assessment = query.get()
     random_assessment.square_text = random_assessment.square_text.split('\n')
@@ -16,15 +24,13 @@ def square_assessment():
                            random_assessment=random_assessment,
                            form=form)
 
-@kifa_bp.route('/rate_square/<assessment_id>/', methods=['POST'])
 def rate_square(assessment_id):
-
-    if request.method == 'POST':
-        q = Assessment.update({Assessment.label_0: int(request.form['label_0']),
-                               Assessment.label_1: int(request.form['label_1']),
-                               Assessment.label_2: int(request.form['label_2']),
-                               Assessment.label_3: int(request.form['label_3']),
-                               Assessment.label_4: int(request.form['label_4']),
-                               Assessment.label_5: int(request.form['label_5'])}).where(Assessment.id==assessment_id)
-        q.execute()
-        return redirect(url_for('.square_assessment'))
+    print('Rating id: ', assessment_id)
+    q = Assessment.update({Assessment.label_0: int(request.form['label_0']),
+                           Assessment.label_1: int(request.form['label_1']),
+                           Assessment.label_2: int(request.form['label_2']),
+                           Assessment.label_3: int(request.form['label_3']),
+                           Assessment.label_4: int(request.form['label_4']),
+                           Assessment.label_5: int(request.form['label_5'])}).where(Assessment.id==assessment_id)
+    q.execute()
+    return redirect(url_for('.assessment_random_square'))
